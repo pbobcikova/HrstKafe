6cae6dc0100d578f17203100e46f4490ede1f14c
Petra Bobcikova <pe.hrstkova@seznam.cz>
2023-12-18 18:30:20 +0100

    Instalace Eleventy, příprava šablony a scss

---
+++ eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
